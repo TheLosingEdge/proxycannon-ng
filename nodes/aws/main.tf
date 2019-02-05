f8386de0524d8cbd06b9dd680e03c6efab91bd68
TheL0singEdge <32302028+TheLosingEdge@users.noreply.github.com>
2019-02-05 12:32:19 -0600

    Update main.tf

--- nodes/aws/main.tf
+++ nodes/aws/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "exit-node" {
-  ami           = "ami-0f65671a86f061fcd"
+  ami           = "ami-0028983ab738d552e"
   instance_type = "t2.micro"
   key_name      = "proxycannon"
   vpc_security_group_ids = ["${aws_security_group.exit-node-sec-group.id}"]
